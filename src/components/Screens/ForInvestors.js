4f113b6e93c44942bc668c27495014783b427699
Max Petruck <petruck.maxim@gmail.com>
2015-12-19 00:04:38 +0200

    Add empty component for ‘For investors’ screen

---
+++ src/components/Screens/ForInvestors.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "src/styles/Screens/ForInvestors.css";
+
+class ForInvestors extends React.Component {
+  render () {
+    return (
+      <section className="section">
+      </section>
+    );
+  }
+}
+
+export default ForInvestors;
