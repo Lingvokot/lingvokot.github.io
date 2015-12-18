7148d6325d9e4a3d99f10cb6e63ea3773fa44a51
Max Petruck <petruck.maxim@gmail.com>
2015-12-19 00:01:00 +0200

    Add empty component for ‘OurWorld’ screen

---
+++ src/components/Screens/OurWorld.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "src/styles/Screens/OurWorld.css";
+
+class OurWorld extends React.Component {
+  render () {
+    return (
+      <section className="section">
+      </section>
+    );
+  }
+}
+
+export default OurWorld;
