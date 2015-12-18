0b98f1d4c8f3816aada7620155964946011e579c
Max Petruck <petruck.maxim@gmail.com>
2015-12-19 00:06:10 +0200

    Add empty footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "src/styles/Footer.css";
+
+class Footer extends React.Component {
+  render () {
+    return (
+      <footer>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
