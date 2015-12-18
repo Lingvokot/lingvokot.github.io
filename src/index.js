811d546863a914d964e2b21e891e3a41d52d4ca9
Max Petruck <petruck.maxim@gmail.com>
2015-12-18 13:32:01 +0200

    Change root component to App.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ReactDOMServer from "react-dom/server";
 
-import Foo from "src/components/Foo.js";
+import App from "src/components/App.js";
 
 // are we running in DOM environment?
 global.IS_CLIENT = typeof document !== "undefined";
@@ -10,7 +10,7 @@ global.IS_CLIENT = typeof document !== "undefined";
 let render = new Function(); // server renderer as noop by default
 
 // this element will be rendered both for client and server side.
-const RootElement = <Foo />;
+const RootElement = <App />;
 
 if (global.IS_CLIENT) {
   ReactDOM.render(
