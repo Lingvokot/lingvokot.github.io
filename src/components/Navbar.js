82c44d7f244dbfa560a70039d43f85172c154fb9
Max Petruck <petruck.maxim@gmail.com>
2015-12-19 00:05:32 +0200

    Add empty navigation bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "src/styles/Navbar.css";
+
+class Navbar extends React.Component {
+  render () {
+    return (
+      <nav>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
