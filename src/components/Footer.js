0d7dbe898cddc12eec875df15b23898b2bae51a6
Max Petruck <petruck.maxim@gmail.com>
2015-12-19 14:42:43 +0200

    Add footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,38 @@
 import React from "react";
+
+import Navigation from "./Navigation.js";
 import "src/styles/Footer.css";
 
 class Footer extends React.Component {
   render () {
     return (
-      <footer>
+      <footer className="footer">
+        <div className="footer__content">
+          <div>
+            <div className="footer-socials">
+              <div className="footer-socials__github">
+                <a href="https://github.com/Lingvokot">
+                  <img src="src/img/footer/github-ico.svg"/>
+                </a>
+              </div>
+              <div className="footer-socials__facebook">
+                <a href=""><img src="src/img/footer/facebook-ico.svg"/></a>
+              </div>
+              <div className="footer-socials__slideshare">
+                <a href=""><img src="src/img/footer/slideshare-ico.svg"/></a>
+              </div>
+            </div>
+            <div>
+              <p className="footer-text">
+                © 2015 LingvoKot inc.<br/>
+                All rights reserved
+              </p>
+            </div>
+          </div>
+          <div>
+            <Navigation classes="navigation__page-scroller--black" />
+          </div>
+        </div>
       </footer>
     );
   }
