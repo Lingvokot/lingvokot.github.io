78c8b42ad6030b8f3d1e4be7a5f7aa544872d9b6
Max Petruck <petruck.maxim@gmail.com>
2015-12-19 14:39:59 +0200

    Add Navbar component content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,20 @@
 import React from "react";
+
+import Navigation from "./Navigation.js";
 import "src/styles/Navbar.css";
 
 class Navbar extends React.Component {
   render () {
     return (
-      <nav>
+      <nav className="navbar">
+        <div className="navbar__content">
+          <div className="logo">
+            <img src="src/img/navbar/logo.svg" />
+          </div>
+          <div>
+            <Navigation classes="navigation__page-scroller--green"/>
+          </div>
+        </div>
       </nav>
     );
   }
