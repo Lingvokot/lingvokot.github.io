300aee691e8fd0dc6fc1dd2d83ceaa41793b98de
Max Petruck <petruck.maxim@gmail.com>
2015-12-23 23:31:25 +0200

    Split big header into two parts and edit text

--- src/components/Screens/ForInvestors.js
+++ src/components/Screens/ForInvestors.js
@@ -9,10 +9,12 @@ class ForInvestors extends React.Component {
       <section className="section">
         <div className="section__content section__content-for-investors">
           <ContactForm />
-          <div className="for-investors--description">
+          <div className="for-investors-description">
             <h2 className="header header--level-2">
               <div className="header--big">
-                Great opportunities<br/>
+                Great opportunities
+              </div>
+              <div className="header--big">
                 for your investments<br/>
               </div>
               <div className="header--small">
@@ -20,20 +22,20 @@ class ForInvestors extends React.Component {
               </div>
             </h2>
             <p className="text">
-              Inspired by minimalist art, Dots was created<br/>
-              with the notion that beauty and fun are not mutually<br/>
+              Inspired by minimalist art, Dots was created with<br/>
+              the notion that beauty and fun are not mutually<br/>
               exclusive. Dots is a minimalist game that was de-<br/>
               signed to be a soothing experience. But dig<br/>
-              deeper and there is a depth of challenging gameplay<br/>
-              for more competitive players.
+              deeper and there is a depth of challenging game-<br/>
+              play for more competitive players.
             </p>
             <p className="text">
-              Inspired by minimalist art, Dots was created<br/>
-              with the notion that beauty and fun are not mutually<br/>
+              Inspired by minimalist art, Dots was created with<br/>
+              the notion that beauty and fun are not mutually<br/>
               exclusive. Dots is a minimalist game that was de-<br/>
               signed to be a soothing experience. But dig<br/>
-              deeper and there is a depth of challenging gameplay<br/>
-              for more competitive players.
+              deeper and there is a depth of challenging game-<br/>
+              play for more competitive players.
             </p>
           </div>
         </div>
