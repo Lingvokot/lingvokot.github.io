37f365a39f160040b293e956cbedf98c21bbaf41
Max Petruck <petruck.maxim@gmail.com>
2015-12-28 17:20:18 +0200

    Refactor Navigation, change boolean props usage

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -11,22 +11,22 @@ class Navigation extends React.Component {
     return (
         <ul className="navigation">
           <li className={className}>
-            <Link to="apps" spy={true} smooth={true} duration={200}>
+            <Link to="apps" spy smooth duration={200}>
               Applications
             </Link>
           </li>
           <li className={className}>
-            <Link to="tech" spy={true} smooth={true} duration={200}>
+            <Link to="tech" spy smooth duration={200}>
               Technologies
             </Link>
           </li>
           <li className={className}>
-            <Link to="socials" spy={true} smooth={true} duration={200}>
+            <Link to="socials" spy smooth duration={200}>
               Socials
             </Link>
           </li>
           <li className={className}>
-            <Link to="investors" spy={true} smooth={true} duration={200}>
+            <Link to="investors" spy smooth duration={200}>
               For investors
             </Link>
           </li>
