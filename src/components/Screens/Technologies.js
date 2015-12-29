e95b99afe9626a9d7371dbde3e4b383fa9960df5
Max Petruck <petruck.maxim@gmail.com>
2015-12-29 19:03:56 +0200

    Add html5 semantic tags

--- src/components/Screens/Technologies.js
+++ src/components/Screens/Technologies.js
@@ -5,7 +5,7 @@ class Technologies extends React.Component {
   render () {
     return (
       <section className="section">
-        <div className="section__content section__content--technologies">
+        <section className="section__content section__content--technologies">
           <h2 className="header header--level-2">
             <div className="header--big">We like what we do<br/></div>
             <div className="header--small">
@@ -13,13 +13,13 @@ class Technologies extends React.Component {
               Discover what is behind our apps
             </div>
           </h2>
-          <div className="technologies-container">
-            <div className="tech">
-              <div>
-                <div className="gear">
+          <section className="technologies-container">
+            <section className="tech">
+              <section>
+                <figure className="gear">
                   <img src="src/img/technologies/gear.svg"/>
-                </div>
-                <div className="tech__description">
+                </figure>
+                <article className="tech__description">
                   <h3 className="header header--level-3">CI & CD</h3>
                   <p className="text text--centered">
                     We use fully automated<br/>
@@ -28,16 +28,16 @@ class Technologies extends React.Component {
                     can deliver any changes<br/>
                     rapidly
                   </p>
-                </div>
-              </div>
+                </article>
+              </section>
               <a className="tech-link" href="">Our assets</a>
-            </div>
-            <div className="tech">
-              <div>
-                <div className="react-native">
+            </section>
+            <section className="tech">
+              <section>
+                <figure className="react-native">
                   <img src="src/img/technologies/react-native.svg"/>
-                </div>
-                <div className="tech__description">
+                </figure>
+                <article className="tech__description">
                   <h3 className="header header--level-3">React native</h3>
                   <p className="text">
                     We build native iOS and<br/>
@@ -47,16 +47,16 @@ class Technologies extends React.Component {
                     efficient way. Also hot deploy is<br/>
                     available.
                   </p>
-                </div>
-              </div>
+                </article>
+              </section>
               <a className="tech-link" href="">Read more</a>
-            </div>
-            <div className="tech">
-              <div>
-                <div className="tech-talks">
+            </section>
+            <section className="tech">
+              <section>
+                <figure className="tech-talks">
                   <img src="src/img/technologies/tech-talks.svg"/>
-                </div>
-                <div className="tech__description">
+                </figure>
+                <article className="tech__description">
                   <h3 className="header header--level-3">Tech Talks</h3>
                   <p className="text">
                     We use cutting edge<br/>
@@ -65,12 +65,12 @@ class Technologies extends React.Component {
                     always ready to share our<br/>
                     vision.
                   </p>
-                </div>
-              </div>
+                </article>
+              </section>
               <a className="tech-link" href="">Our presentations</a>
-            </div>
-          </div>
-        </div>
+            </section>
+          </section>
+        </section>
       </section>
     );
   }
