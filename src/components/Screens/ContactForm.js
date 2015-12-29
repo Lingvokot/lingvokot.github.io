8b3b15b5e1c39e8461131de5dda9a270a3ff99ac
ColCh <colchgh@gmail.com>
2015-12-29 17:46:24 +0300

    Integrate MailChimp

--- src/components/Screens/ContactForm.js
+++ src/components/Screens/ContactForm.js
@@ -5,19 +5,22 @@ import "src/styles/Screens/ContactForm.css";
 class Screens extends React.Component {
   render () {
     return (
-      <form>
+      <form action="http://lingvokot.us12.list-manage.com/subscribe/post" method="POST">
+
+        <input type="hidden" name="u" value="4ee96ad6ec49116c3de5ed5b5" />
+        <input type="hidden" name="id" value="0ff14c223a" />
+        <input type="hidden" name="b_4ee96ad6ec49116c3de5ed5b5_0ff14c223a" tabIndex="-1" value="" />
+
         <div className="contact-form--container">
           <div className="contact-form__input-part">
             <label htmlFor="name" className="label">Name</label>
-            <input id="name" className="input-form" type="text"/>
+            <input id="name" className="input-form" type="text" name="MERGE1" size="25" />
             <label htmlFor="site" className="label">Site</label>
-            <input id="site" className="input-form" type="text"/>
+            <input id="site" className="input-form" type="text" name="MERGE2" size="25" />
             <label htmlFor="email" className="label">E-mail</label>
-            <input id="email" className="input-form" type="text"/>
+            <input id="email" className="input-form" type="email" autoCapitalize="off" autoCorrect="off" name="MERGE0" size="25" />
             <label htmlFor="promotion" className="checkbox">
-              <input id="promotion" type="checkbox" name="_promotion"
-                  value="promotion"
-              />
+              <input id="promotion" type="checkbox" name="group[2105][1]" />
               <span className="checkbox-img"></span>
               <span className="checkbox-label">
                 We have expertise in the marketing<br/>
@@ -26,7 +29,7 @@ class Screens extends React.Component {
             </label>
           </div>
           <div className="contact-form__submit-part">
-            <input type="submit" className="submit-button" value="Send"/>
+            <input type="submit" className="submit-button" name="submit" value="Send"/>
           </div>
         </div>
       </form>
