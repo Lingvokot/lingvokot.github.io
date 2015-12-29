f3508ea62556c895ba95d0de18d1f56a9d2efa3d
Max Petruck <petruck.maxim@gmail.com>
2015-12-29 19:15:08 +0200

    Add html semantic tags

--- src/components/Screens/OurApps.js
+++ src/components/Screens/OurApps.js
@@ -5,13 +5,13 @@ class OurApps extends React.Component {
   render () {
     return (
       <section className="section section--green">
-        <div className="section__content">
-          <div className="slider">
-            <div className="devices">
+        <section className="section__content">
+          <section className="slider">
+            <figure className="devices">
               <img src="src/img/apps/devices.svg"/>
-            </div>
-            <div className="slider__right-part">
-              <div>
+            </figure>
+            <section className="slider__right-part">
+              <article>
                 <h2 className="header header--level-2">
                   <div className="header--big">Applications</div>
                   <div className="header--small header--heavy">
@@ -27,24 +27,24 @@ class OurApps extends React.Component {
                   minimalist game that was designed to be a soothing experience.
                   But dig deeper and there is a depth of challenging
                 </p>
-              </div>
-              <div className="store-buttons">
-                <div className="app-store">
+              </article>
+              <ul className="store-buttons">
+                <li className="app-store">
                   <a className="store-link" href="">
                     <img src="src/img/apps/app-store.svg"/>
                   </a>
                   <img src="src/img/apps/app-store-button-shadow.svg"/>
-                </div>
-                <div className="google-play">
+                </li>
+                <li className="google-play">
                   <a className="store-link" href="">
                     <img src="src/img/apps/google-play.svg"/>
                   </a>
                   <img src="src/img/apps/google-play-button-shadow.svg"/>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+                </li>
+              </ul>
+            </section>
+          </section>
+        </section>
       </section>
     );
   }
