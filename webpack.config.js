88fc88e0c557520e38665f54bb889c75263f39d6
ColCh <colchgh@gmail.com>
2015-12-29 15:05:20 +0300

    Remove postcss loader

--- webpack.config.js
+++ webpack.config.js
@@ -24,13 +24,13 @@ var variables = {
   module_loaders: {
     "prod": [
       { test: /(\.jsx)|(\.js)$/, include: path.join(__dirname, "src"), loader: "babel-loader" },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!postcss-loader") },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
       { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'url-loader?limit=10000!img-loader?progressive=true' },
       { test: /fonts\/.*\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
     ],
     "dev": [
       { test: /(\.jsx)|(\.js)$/, include: path.join(__dirname, "src"), loader: "react-hot-loader!babel-loader" },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!postcss-loader") },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
       { test: /\.png$/, loader: "url-loader?limit=100000" },
       { test: /\.jpe?g$/, loader: "file-loader" },
       { test: /\.svg$/, loader: "file-loader" },
@@ -53,15 +53,6 @@ var variables = {
     'dev': [
       new webpack.NoErrorsPlugin(),
       new ExtractTextPlugin("[name].css")
-    ]
-  },
-  postcss_plugins: {
-    "prod": [
-      cssnano,
-      autoprefixer
-    ],
-    "dev": [
-      autoprefixer
     ]
   }
 };
@@ -102,10 +93,6 @@ module.exports = {
     modulesDirectories: ["node_modules"],
     alias: {},
     root: __dirname
-  },
-
-  postcss: function () {
-      return variables.postcss_plugins[BUILD_TYPE];
   }
 
 };
