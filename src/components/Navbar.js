4cee4239f398870c4b6dddbe53d74a6e0dca9779
Max Petruck <petruck.maxim@gmail.com>
2015-12-30 18:08:45 +0200

    Add offset computation and transferring it to Navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import Navigation from "./Navigation.js";
 import "src/styles/Navbar.css";
 
 class Navbar extends React.Component {
+  componentDidMount() {
+    Navbar.offset = -ReactDOM.findDOMNode(this).offsetHeight;
+    this.forceUpdate();
+  }
   render () {
     return (
       <nav className="navbar">
@@ -12,7 +17,9 @@ class Navbar extends React.Component {
             <img src="src/img/navbar/logo.svg" />
           </div>
           <div className="navigation-container">
-            <Navigation classes="navigation__page-scroller--green"/>
+            <Navigation classes="navigation__page-scroller--green"
+                offset={Navbar.offset}
+            />
           </div>
         </div>
       </nav>
