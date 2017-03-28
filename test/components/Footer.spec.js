e9e78797574326faff4c80bd87c26e5a3b4e2681
keshan3262 <keshan3262@gmail.com>
2017-03-28 17:12:37 +0300

    Some tests for Footer added

--- test/components/Footer.spec.js
+++ test/components/Footer.spec.js
@@ -4,39 +4,47 @@ import ReactDOM from "react-dom";
 import Footer from "src/components/Footer.js";
 
 describe("Footer", () => {
-  var frame, container, root, rootElement, element;
-
+  var frame, rootElement, element;
   before(function(done) {
     frame = quixote.createFrame({}, done);
   });
-
   after(function() {
     frame.remove();
   });
-
   beforeEach(function() {
     frame.reset();
-
-    container = frame.add('<div id="app-container"></div>', "container");
-    root = frame.get("#app-container", "react root element");
+    let container = frame.add('<div id="app-container"></div>', "container");
+    let root = frame.get("#app-container", "react root element");
     rootElement = root.toDomElement();
-    console.log("rootElement");
-    console.log(rootElement);
 
     ReactDOM.render(
       <Footer />,
       rootElement
     );
-
     element = frame.get(".footer");
-    console.log("Element");
-    console.log(element);
   });
 
-  it('for it to be', () => {
-    console.log("for it to be");
-    let k = 2 * 2;
-    k.should.equal(4);
+  it('Footer leaves no spaces at the left, right and bottom sides of the page', () => {
+    let pageBody = frame.body();
+    element.assert({
+      left: pageBody.left,
+      right: pageBody.right,
+      bottom: pageBody.bottom
+    });
   });
 
+  it("Child elements are centered", () => {
+    let expectedCenterX = pageBody.width / 2;
+    let childrenContainer = frame.get(".footer .ui.three.column.grid.footer-socials");
+    let childrenContainerPosition = childrenContainer.getRawPosition();
+    expect((childrenContainerPosition.left + childrenContainerPosition.right) / 2)
+          .to.equal(expectedCenterX);
+    let textLabels = frame.getAll(".footer span");
+    expect(textLabelsInList.length()).to.equal(2);
+    for (let i = 0; i < textLabelsInList.length(); i++) {
+      let label1 = textLabelsInList.at(i);
+      let labelPosition = label1.getRawPosition();
+      expect((labelPosition.left + labelPosition.right) / 2).to.equal(expectedCenterX);
+    }
+  });
 });
