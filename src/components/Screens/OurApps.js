a800fdd04b048489e58143e9866c589ebbaec50a
keshan3262 <keshan3262@gmail.com>
2017-09-11 17:19:36 +0300

    One more entry added

--- src/components/Screens/OurApps.js
+++ src/components/Screens/OurApps.js
@@ -72,6 +72,35 @@ t9TViGLILYPinunLF4IDE8iL6usM-J4-NY=h900"
         ],
         name: "Break a Code",
         bundleId: "com.lingvokot.mastermind"
+      }, {
+        icon: "https://lh3.googleusercontent.com/cwhsC3w85YWdnyfIvbnzMRqWcXLC9\
+Ycl_ucf9OxStQdlD6utEQiw0uNr3tqSV3NFVG8=w300",
+        description: "Free logical board game for all ages Break a Code. A \
+variant of popular game Mastermind.\n\
+Challenge yourself and your abilities to solve ciphers. An excellent \
+educational game for children and adults.\n\
+\n\
+In this game, you have to solve the computer-generated code with a certain \
+number of moves.\n\
+Why would you like this game?\n\
+- A large selection of levels and complex combinations for experienced players\n\
+- The ability to play a quick occasional game\n\
+- Compete with yourself and get the best level time\n\
+- A great tool to relax when your brain is overstressed\n\
+- “Bulls and Cows” maniacs will enjoy this version of the game with a nice \
+interface",
+        screenshotUrls: [
+          "https://lh3.googleusercontent.com/yCHmD2yT1Mu2wC8Ilh_pO0911SBCj7IZS\
+UlYMAuPGiuIb8oOCdCd_ga14Am_n2Up_FM=h900",
+          "https://lh3.googleusercontent.com/JJfWEMgxKnYGdp1TInC1j5n8MAg8lkXGd\
+RK5Mbw86-odlLgO5UJYTVpvVVKTl-_u-vo=h900",
+          "https://lh3.googleusercontent.com/ZqFgtxQDi9fdWcl4uUQYrXis7JBJvZD9p\
+4w1za4jBDl_l-bMNgRcf8LFeXHuAEdsWF0=h900",
+          "https://lh3.googleusercontent.com/BfnRz5u_PZ_DIPotSyIAo5YWjFbu6GqN-\
+90VcK-RYBl08RnfQOhV9hZ66KhSP0PKXbw=h900"
+        ],
+        name: "Mastermind Code",
+        bundleId: "com.lingvokot.mastermind.en"
       }]
     });
   }
